bbcd065d4bd6cb7e1cd6ae2a8fc0ea4b35746de8
Charlie Dudzik <dudzikc@outlook.com>
2026-03-15 13:15:18 -0500

    Update

--- rs/src/config.rs
+++ rs/src/config.rs
@@ -1,3 +1,5 @@
+use serde_json::Value;
+
 use crate::test_step::TestStepGroup;
 use std::collections::HashMap;
 use std::path::PathBuf;
@@ -7,6 +9,23 @@ struct ConfigData {
     path: PathBuf,
     parent: Option<Rc<ConfigData>>,
     step_sets: HashMap<String, TestStepGroup>,
+    data: Value,
 }
 
-impl ConfigData {}
+impl ConfigData {
+    pub fn get_step_set(&self, key: String) -> Option<&TestStepGroup> {
+        let retrieved_value = self.step_sets.get(&key);
+        match retrieved_value {
+            Some(value) => {
+                return Some(value);
+            }
+            None => match &self.parent {
+                Some(parent) => parent.get_step_set(key),
+                None => None,
+            },
+        }
+    }
+
+    pub fn get_keys(&self, Vec<String>) Value {
+    }
+}
