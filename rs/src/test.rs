3291fa22866dc4476d5061c74b6dc601af2e28a9
Charlie Dudzik <dudzikc@outlook.com>
2026-03-19 23:03:07 -0500

    Update

--- rs/src/test.rs
+++ rs/src/test.rs
@@ -1,5 +1,18 @@
+use std::fs::File;
+use std::path::PathBuf;
 
 pub struct Test {
-    name: String
+    name: String,
 }
 
+impl Test {
+    pub fn load_test_file(path: &PathBuf) {
+        let file = File::open(path)?;
+
+        // 2. Wrap it in a BufReader for efficiency
+        let reader = BufReader::new(file);
+
+        // 3. Deserialize directly from the reader
+        let config: Config = serde_yaml::from_reader(reader)?;
+    }
+}
