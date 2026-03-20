5f232ae975f2ba4695b7611e6d56df47e4ad0416
Charlie Dudzik <dudzikc@outlook.com>
2026-03-20 11:38:40 -0500

    Update

--- rs/src/test.rs
+++ rs/src/test.rs
@@ -1,10 +1,10 @@
 use serde::Deserialize;
-use serde_yaml::Value;
+use serde_yaml::{Value, from_value};
 use std::fs::File;
 use std::io::BufReader;
 use std::path::PathBuf;
 
-use crate::test_spec::TestSpec;
+use crate::test_spec::{ConfigSpec, TestSpec};
 
 pub struct Test {
     name: String,
@@ -20,6 +20,19 @@ impl Test {
             match test_file_result {
                 Ok(tests_file) => {
                     println!("Loaded Test File");
+
+                    if let Some(config_data) = tests_file.get("config") {
+                        match from_value::<ConfigSpec>(config_data.clone()) {
+                            Ok(config_spec) => {
+                                println!("Loaded Config Data: {:?}", config_spec);
+                                // use config_spec here
+                            }
+                            Err(e) => {
+                                panic!("Failed to parse test config: {}\n{}", path.display(), e);
+                                // eprintln!("Failed to parse test config: {}", e);
+                            }
+                        }
+                    }
                 }
                 Err(e) => {
                     eprintln!("{}", e);
